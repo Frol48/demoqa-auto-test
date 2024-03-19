71a3af07f31123d6334d6fea4075c6c1b741f911
froll_1 <dro@renue.ru>
2024-03-19 22:56:27 +0300

    add Configuration.remote

--- src/test/java/RegistrationFormTests.java
+++ src/test/java/RegistrationFormTests.java
@@ -16,6 +16,7 @@ public class RegistrationFormTests {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
